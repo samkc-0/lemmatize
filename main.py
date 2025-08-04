a79219270a4e414b98dbb9b46235f532e3ea723e
samkc-0 <sam.k.c.0@icloud.com>
2025-08-04 16:10:56 +0100

    add demo page

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from routers import lexicography
 
 app = FastAPI()
 app.include_router(lexicography.router, prefix="/lexicography")
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+def serve_index():
+    return FileResponse("static/index.html")
