f2e36673d27b061fd038bc07039f6fd4b048d345
samkc-0 <sam.k.c.0@icloud.com>
2025-08-08 16:53:29 +0100

    remove jinja2 import

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 from routers import lexicography
 
 app = FastAPI()
 app.include_router(lexicography.router, prefix="/lexicography")
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
-
-templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
